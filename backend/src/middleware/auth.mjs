8d86dfc058b3c0e81fc4e691d0ee57f312151e04
TyTheTechie <github@aiohomelab.com>
2023-10-18 19:50:05 -0500

    Added JWT Token Logic

--- backend/src/middleware/auth.mjs
+++ backend/src/middleware/auth.mjs
@@ -1,7 +1,18 @@
 import jwt from 'jsonwebtoken';
 
 const authenticate = (req, res, next) => {
-  // Handle authentication logic...
+  // Get the token from the header
+  const token = req.header('auth-token');
+  if (!token) return res.status(401).send('Access Denied');
+
+  try {
+    // Verify the token
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next(); // Continue to the next middleware/route handler
+  } catch (err) {
+    res.status(400).send('Invalid Token');
+  }
 };
 
 export default authenticate;
