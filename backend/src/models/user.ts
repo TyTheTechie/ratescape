efbae86b62a766153273fe9a35ab1a9fcfe50d01
TyTheTechie <github@aiohomelab.com>
2023-10-18 20:24:56 -0500

    new routes

---
+++ backend/src/models/user.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 255
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  }
+});
+
+export default mongoose.model('User', userSchema);
