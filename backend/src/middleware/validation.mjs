197df05e94c357ccdddb2fa1848371026f98392c
TyTheTechie <github@aiohomelab.com>
2023-10-19 21:35:18 -0500

    sign up validation correction

--- backend/src/middleware/validation.mjs
+++ backend/src/middleware/validation.mjs
@@ -3,6 +3,7 @@ import Joi from 'joi';
 export const signupValidation = (data) => {
   const schema = Joi.object({
     fullName: Joi.string().min(3).max(255).required(),
+    username: Joi.string().min(3).max(255).required(),
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required(),
   });
