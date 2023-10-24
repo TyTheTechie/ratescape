04984ccbb7b9f5bc294a56bfe469f60cf5a58e40
TyTheTechie <github@aiohomelab.com>
2023-10-23 21:25:07 -0500

    User Profiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { io } from "socket.io-client";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:5000"); // Replace 5000 with your backend port if different
+
+    socket.on("newReview", (review: any) => {
+      // Update the product's average rating in the UI
+      // For now, we'll just log the new review to the console
+      console.log("New review received:", review);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ frontend/src/UserProfile.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+type Review = {
+  _id: string;
+  productName: string;
+  rating: number;
+  reviewText: string;
+};
+
+const UserProfile: React.FC = () => {
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [editingReview, setEditingReview] = useState<Review | null>(null);
+  const [editedReviewText, setEditedReviewText] = useState('');
+  const [editedRating, setEditedRating] = useState<number>(0);
+
+  useEffect(() => {
+    async function fetchUserReviews() {
+      try {
+        const response = await axios.get('/api/users/myreviews');
+        setReviews(response.data);
+      } catch (error) {
+        console.error('Error fetching user reviews:', error);
+      }
+    }
+
+    fetchUserReviews();
+  }, []);
+
+  const handleEditClick = (review: Review) => {
+    setEditingReview(review);
+    setEditedReviewText(review.reviewText);
+    setEditedRating(review.rating);
+  };
+
+  const handleSaveClick = async () => {
+    if (editingReview) {
+      try {
+        const updatedReview = {
+          ...editingReview,
+          reviewText: editedReviewText,
+          rating: editedRating,
+        };
+        await axios.put(`/api/reviews/${editingReview._id}`, updatedReview);
+        setReviews((prevReviews) =>
+          prevReviews.map((review) =>
+            review._id === editingReview._id ? updatedReview : review
+          )
+        );
+        setEditingReview(null);
+      } catch (error) {
+        console.error('Error updating review:', error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h2>Your Reviews</h2>
+      {reviews.map((review) => (
+        <div key={review._id}>
+          <h3>{review.productName}</h3>
+          <p>Rating: {review.rating} stars</p>
+          <p>{review.reviewText}</p>
+          <button onClick={() => handleEditClick(review)}>Edit</button>
+        </div>
+      ))}
+      {editingReview && (
+        <div className="modal">
+          <h3>Edit Review for {editingReview.productName}</h3>
+          <textarea
+            value={editedReviewText}
+            onChange={(e) => setEditedReviewText(e.target.value)}
+          ></textarea>
+          <select
+            value={editedRating}
+            onChange={(e) => setEditedRating(Number(e.target.value))}
+          >
+            <option value={1}>1 Star</option>
+            <option value={2}>2 Stars</option>
+            <option value={3}>3 Stars</option>
+            <option value={4}>4 Stars</option>
+            <option value={5}>5 Stars</option>
+          </select>
+          <button onClick={handleSaveClick}>Save</button>
+          <button onClick={() => setEditingReview(null)}>Cancel</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
