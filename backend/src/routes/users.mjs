eda6045b209354605f49907eb77176bcb17f6fad
TyTheTechie <github@aiohomelab.com>
2023-10-23 22:04:17 -0500

    small tweak

--- backend/src/routes/users.mjs
+++ backend/src/routes/users.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.mjs';
+import Review from '../models/review.mjs'; // Import the Review model
 import authenticate from '../middleware/auth.mjs';
 import sendEmail from '../utils/emailUtil.mjs';
 
@@ -26,6 +27,50 @@ router.get('/verify-email', async (req, res) => {
   }
 });
 
-// ... other user routes ...
+// Fetching User Reviews
+router.get('/myreviews', authenticate, async (req, res) => {
+    try {
+        const reviews = await Review.find({ user: req.user.id });
+        res.json(reviews);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// Editing a Review
+router.put('/myreviews/:id', authenticate, async (req, res) => {
+    const { productName, rating, reviewText, productUrl } = req.body;
+    const reviewFields = { productName, rating, reviewText, productUrl };
+
+    try {
+        let review = await Review.findById(req.params.id);
+        if (!review) return res.status(404).json({ msg: 'Review not found' });
+        if (review.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        review = await Review.findByIdAndUpdate(req.params.id, { $set: reviewFields }, { new: true });
+        res.json(review);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// Deleting a Review
+router.delete('/myreviews/:id', authenticate, async (req, res) => {
+    try {
+        const review = await Review.findById(req.params.id);
+        if (!review) return res.status(404).json({ msg: 'Review not found' });
+        if (review.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        await Review.findByIdAndRemove(req.params.id);
+        res.json({ msg: 'Review removed' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 export default router;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from 'react';
 import { io } from "socket.io-client";
-import logo from './logo.svg';
-import './App.css';
+import UserProfile from './UserProfile';
 
 function App() {
+  // Establish a WebSocket connection to listen for new reviews
   useEffect(() => {
-    const socket = io("http://localhost:5000"); // Replace 5000 with your backend port if different
+    const socket = io("http://localhost:5000");
 
     socket.on("newReview", (review: any) => {
-      // Update the product's average rating in the UI
-      // For now, we'll just log the new review to the console
       console.log("New review received:", review);
     });
 
@@ -19,21 +17,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen">
+      <nav className="bg-blue-600 p-4 text-white">
+        <ul className="flex space-x-4 justify-center">
+          <li><a href="/" className="hover:underline">Home</a></li>
+          <li><a href="/profile" className="hover:underline">Profile</a></li>
+          <li><a href="/reviews" className="hover:underline">Reviews</a></li>
+          <li><a href="/about" className="hover:underline">About</a></li>
+        </ul>
+      </nav>
+      <main className="p-4">
+        <h1 className="text-4xl font-bold mb-4 text-center">Welcome to RateScape</h1>
+        <p className="text-xl mb-4 text-center">Your one-stop platform for product reviews.</p>
+        <UserProfile />
+      </main>
     </div>
   );
 }
