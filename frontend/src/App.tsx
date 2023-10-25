3f73851f9e38836c15bc8b2d8fee47652b947689
TyTheTechie <github@aiohomelab.com>
2023-10-25 06:37:15 -0500

    small tweak

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UserProfile from './pages/UserProfile';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-import RegistrationPage  from './pages/RegistrationPage';
+import RegistrationPage from './pages/RegistrationPage';
 import { io } from 'socket.io-client';
 
 function App() {
@@ -29,12 +29,14 @@ function App() {
           <ul className="flex space-x-4 justify-center">
             <li><Link to="/" className="hover:underline">Home</Link></li>
             <li><Link to="/login" className="hover:underline">Login</Link></li>
+            <li><Link to="/register" className="hover:underline">Register</Link></li>
             {isLoggedIn && <li><Link to="/profile" className="hover:underline">Profile</Link></li>}
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
           <Route path="/profile" element={<UserProfile />} />
         </Routes>
       </div>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,53 +1,92 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
 
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post('/api/auth/login', { email, password });
-      if (response.data.token) {
-        // Store the token in local storage (or session storage)
-        localStorage.setItem('authToken', response.data.token);
-        // Redirect to the main page or dashboard
-        window.location.href = '/';
-      }
-    } catch (err) {
-      setError('Invalid email or password');
-    }
-  };
+    const handleLogin = async () => {
+        try {
+            const response = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+            const data = await response.json();
+            if (data.token) {
+                localStorage.setItem('authToken', data.token);
+                window.location.href = '/';
+            }
+        } catch (err) {
+            setError('Invalid email or password');
+        }
+    };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        {error && <p className="text-red-500 mb-4">{error}</p>}
-        <input
-          type="email"
-          placeholder="Email"
-          className="border p-2 w-full mb-4"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="border p-2 w-full mb-4"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          className="bg-blue-600 text-white p-2 w-full rounded"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-md w-96 transform transition-transform duration-500 hover:scale-105">
+                <h2 className="text-2xl font-bold mb-4 text-center">Login to RateScape</h2>
+                {error && <p className="text-red-500 mb-4 text-center">{error}</p>}
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <div className="flex items-center border rounded w-full py-2">
+                        <span className="px-3 text-gray-500">
+                            <i className="fas fa-envelope"></i>
+                        </span>
+                        <input
+                            type="email"
+                            id="email"
+                            className="flex-1 appearance-none border-none py-2 px-3 text-gray-700 focus:outline-none"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <div className="flex items-center border rounded w-full py-2">
+                        <span className="px-3 text-gray-500">
+                            <i className="fas fa-lock"></i>
+                        </span>
+                        <input
+                            type="password"
+                            id="password"
+                            className="flex-1 appearance-none border-none py-2 px-3 text-gray-700 focus:outline-none"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex justify-between items-center mb-4">
+                    <label className="flex items-center">
+                        <input type="checkbox" className="mr-2" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} />
+                        Remember Me
+                    </label>
+                    <a href="#" className="text-blue-500 hover:underline">Forgot Password?</a>
+                </div>
+                <div className="text-center mb-3">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full transform transition-transform duration-500 hover:scale-105"
+                        onClick={handleLogin}
+                    >
+                        Sign in
+                    </button>
+                </div>
+                <p className="text-center">
+                    Not a member? <a href="/register" className="text-blue-500 hover:underline">Register</a>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
